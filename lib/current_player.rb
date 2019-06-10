41d3da09bd8ed2cfdf3bea8b8ab064a522a93ea5
Manny Georganas <manoli.georganas@gmail.com>
2019-06-10 23:32:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,12 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
-
+turn = 0
+board.each do | position|
+  if position % 2 # if its divides evenly it will return 1 or true
+    player = "O"
+    turn += 1
+  else
+    player = 'X'
+  end
 end
