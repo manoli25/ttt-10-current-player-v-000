d622ffc16e2162e13afb1b1097c9a79739077d4c
Manny Georganas <manoli.georganas@gmail.com>
2019-06-10 23:33:06 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,8 +5,8 @@ turn = 0
 board.each do | position|
   if position % 2 # if its divides evenly it will return 1 or true
     player = "O"
-    turn += 1
   else
     player = 'X'
   end
+  turn += 1
 end
