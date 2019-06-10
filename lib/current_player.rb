0ef3270c7c1619a146ef53a348b4798aef650009
Manny Georganas <manoli.georganas@gmail.com>
2019-06-10 23:42:48 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,15 @@ require_relative "../lib/current_player.rb"
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def turn_count(board)
 turn = 0
+turn = turn_count(board)
+if turn % 2
+  player = "0"
+else
+  player = "X"
+end
+
+def turn_count(board)
 board.each do | position |
   turn += 1
 end
