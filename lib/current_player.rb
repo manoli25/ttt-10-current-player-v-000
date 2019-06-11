4f8d3b5e0943ec6a9e29e67308f6bfac5bf7c16e
Manny Georganas <manoli.georganas@gmail.com>
2019-06-11 00:10:53 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,30 @@
+require_relative "../lib/current_player.rb"
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+
+def turn_count(board)
+turn = 0
+
+board.each do |position|
+  if position == "X" || position == "O"
+    turn += 1
+  end
+end
+return turn
+end
+
+
+def current_player(board)
+  turn = turn_count(board)
+
+  turn.even? ? player = "X" : player = "O"
+
+# if turn.even?
+#    player = "X"
+#  else
+#    player = "O"
+#  end
+
+return player
+end
